b60c79cb24f02732f55e9635e11d0412560dbef0
anoopgit7216 <av9692044@gmail.com>
2025-09-23 12:51:24 +0530

    s

--- backend/app/Http/Controllers/api/Supervisor/SupervisorComplaintsController.php
+++ backend/app/Http/Controllers/api/Supervisor/SupervisorComplaintsController.php
@@ -88,7 +88,8 @@ class SupervisorComplaintsController extends Controller
     switch ($userSubrole) {
         case "so-us":
             $query->where('form_status', 1)
-                  ->where('approved_rejected_by_ro', 1);
+                  ->where('approved_rejected_by_ro', 1)
+                  ->Orwhere('approved_rejected_by_ds_js', 0);
                 //   ->where('approved_by_ro', 1);
             // $query->where('complaints.added_by', $user);
             break;
