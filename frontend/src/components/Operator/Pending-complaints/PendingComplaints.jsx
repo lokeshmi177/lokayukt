79121f8e1cee5256d52faff068c1822dc3506e87
krishnaprince7 <princekrishna5707@gmail.com>
2025-10-03 10:42:23 +0530

    Frontend Chgese

--- frontend/src/components/Operator/Pending-complaints/PendingComplaints.jsx
+++ frontend/src/components/Operator/Pending-complaints/PendingComplaints.jsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const BASE_URL = import.meta.env.VITE_API_BASE ?? "http://localhost:8000/api";
 const token = localStorage.getItem("access_token");
-  const subrole = localStorage.getItem("subrole")
+  const subRole = localStorage.getItem("subrole")
 // Create axios instance with token if it exists
 const api = axios.create({
   baseURL: BASE_URL,
